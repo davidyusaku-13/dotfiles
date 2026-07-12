0ad8ec836599e56b1b1a2104d77add2a466c60a6
David Yusaku <davidyusaku13@gmail.com>
2026-07-12 19:44:36 +0700

    fix: allow telescope find_files to see inside hidden .config directories

--- nvim/.config/nvim/after/plugin/telescope.lua
+++ nvim/.config/nvim/after/plugin/telescope.lua
@@ -12,7 +12,7 @@ require("telescope").setup({
 })
 
 local builtin = require("telescope.builtin")
-vim.keymap.set("n", "<leader>ff", builtin.find_files)
+vim.keymap.set("n", "<leader>ff", function() builtin.find_files({ hidden = true }) end)
 vim.keymap.set("n", "<leader>fo", builtin.oldfiles)
 vim.keymap.set("n", "<leader>fq", builtin.quickfix)
 vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
